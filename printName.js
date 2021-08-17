3fa4ba5189ec53226000c77b5962dc24d4936c9a
JoelEkpenyong <joelekpenyong@gmail.com>
2021-08-17 19:18:09 +0100

    #update

--- printName.js
+++ printName.js
@@ -1,1 +1,3 @@
-console.log("My name is Joel Ekpenyong, hngi8 slack username is @thatTechBro")
+// a simple script that pirnts my name to the console.
+
+console.log('My name is Joel Ekpenyong, hngi8 slack username is @thatTechBro');
